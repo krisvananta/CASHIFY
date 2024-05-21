f926a67a13f3325023044db794bca0612f4032dc
krisvananta <afifkrisvananta@gmail.com>
2024-05-21 14:39:44 +0700

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello CASHIFY!");
-        System.out.println("This change is from IntellIJ IDEA!");
+        System.out.println("This change is from IntellIJ IDEA!!");
     }
 }
