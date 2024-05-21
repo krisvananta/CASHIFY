a3410382505be0033cff49c35dc2f1c69f1a9264
krisvananta <afifkrisvananta@gmail.com>
2024-05-21 14:39:44 +0700

    Second Commit

---
+++ src/User.java
@@ -0,0 +1,18 @@
+public class User {
+    private int userId;
+    private String name;
+    private String email;
+    private String password;
+
+    public void login(){
+
+    }
+
+    public void logout(){
+
+    }
+
+    public void register(){
+
+    }
+}
