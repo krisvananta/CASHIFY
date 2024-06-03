e0fd2ae58dd872833c57b00ca527281165e53c9b
AsyifaDzakyy <asyifadzaky.11@gmail.com>
2024-06-03 08:33:16 +0700

    Neww

--- src/Customer.java
+++ src/Customer.java
@@ -1,2 +1,25 @@
-public class Customer {
+public class Customer extends User {
+    private boolean isMember;
+
+    public Customer(int id, String username, String password, boolean isMember) {
+        super(id, username, password, Role.CUSTOMER);
+        this.isMember = isMember;
+    }
+
+    public boolean isMember() {
+        return isMember;
+    }
+
+    public void buyProduct(Product product, int quantity) {
+        double price = product.getPrice() * quantity;
+        if (isMember) {
+            price *= 0.9; // Diskon 10%
+        }
+        System.out.println("Total price: " + price);
+    }
+
+    @Override
+    public void displayDashboard() {
+        System.out.println("Welcome to Customer Dashboard");
+    }
 }
--- src/User.java
+++ src/User.java
@@ -1,18 +1,61 @@
-public class User {
-    private int userId;
-    private String name;
-    private String email;
-    private String password;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-    public void login(){
+public enum Role {
+    CUSTOMER,
+    OWNER,
+    EMPLOYEE
+}
+public abstract class User {
+    public int id;
+    public String username;
+    public String password;
+    public Role role;
 
+    public User(int id, String username, String password, Role role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
     }
 
-    public void logout(){
+    public static User getUserByUsernameAndPassword(String username, String password) {
+        User user = null;
+        String query = "SELECT * FROM Users WHERE username = ? AND password = ?";
 
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+
+            statement.setString(1, username);
+            statement.setString(2, password);
+
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String roleString = resultSet.getString("role");
+                Role role = Role.valueOf(roleString);
+                boolean isMember = resultSet.getBoolean("is_member");
+
+                switch (role) {
+                    case CUSTOMER:
+                        user = new Customer(id, username, password, isMember);
+                        break;
+                    case OWNER:
+                        user = new Owner(id, username, password);
+                        break;
+                    case EMPLOYEE:
+                        user = new Employee(id, username, password);
+                        break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return user;
     }
 
-    public void register(){
-
-    }
+    public abstract void displayDashboard();
 }
